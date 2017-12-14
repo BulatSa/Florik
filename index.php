d4dda9f802b2595e170db431ac0f21bfcdb6d14a
Bulat <sadikovba@gmail.com>
2017-12-14 19:04:07 +0300

    Add 2 new sections

--- index.php
+++ index.php
@@ -77,4 +77,112 @@
 </section>
 
 
+<section class="about-man-sec def-sec">
+	<div class="about-man container">
+		<div class="about-man__info">
+			<div class="block-title">
+				<span>Обо мне</span>
+				<p class="h1">Никита Флоринский</p>
+			</div>
+			<div class="user-content">
+				<p>Я 12 лет занимаюсь digital-маркетингом, 5 лет комплексным  управлением предприятиями в области их коммерческой деятельности. В моём  "портфолио" множество успешных кейсов построения отделов продаж,  лидогенерации, упаковки продуктов. Я работаю как с B2C направлениями,  так и с B2B; разные отрасли: производители одежды, детские товары,  ритейл, оффлайн точки продаж, сетевые компании, системные интеграторы и  многое другое. На этом сайте вы сможете ознакомиться с моими услугами,  кейсами, отзывами и форматами сотрудничества.</p>
+			</div>
+			<div class="about-man__medals">
+				<div class="about-man__medal">
+					<span>Член гильдии маркетологов</span>
+					<img src="img/about-man/medal-1.png" alt="">
+				</div>
+				<div class="about-man__medal">
+					<span>Со-организатор конференции по электронной торговле</span>
+					<img src="img/about-man/medal-2.png" alt="">
+				</div>
+				<div class="about-man__medal">
+					<span>Операционный директор агентства по таргетированной рекламе ВКонтакте</span>
+					<img src="img/about-man/medal-3.png" alt="">
+				</div>
+			</div>
+			<div class="about-man__contact">
+				<span>Следите за мной</span>
+				<div class="about-man__contact-soc">
+					<a href="#" target="_blank"><i class="i-soc-vk"></i></a>
+					<a href="#" target="_blank"><i class="i-soc-fb"></i></a>
+					<a href="#" target="_blank"><i class="i-soc-insta"></i></a>
+					<a href="#" target="_blank"><i class="i-soc-you"></i></a>
+				</div>
+				<a href="#" class="btn btn--trans fancy" data-src="#modal-order">Написать мне</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="certs-sec def-sec">
+	<div class="certs container">
+		<div class="row">
+			<div class="grid-6 grid-12_s">
+				<div class="certs__block">
+					<span>Сертифицированный специалист</span>
+					<div class="certs__block-imgs">
+						<img src="img/serts/specialist-4.jpg" alt="">
+						<img src="img/serts/specialist-1.jpg" alt="">
+						<img src="img/serts/specialist-2.jpg" alt="">
+						<img src="img/serts/specialist-3.jpg" alt="">
+					</div>
+				</div>
+			</div>
+			<div class="grid-6 grid-12_s">
+				<div class="certs__block">
+					<span>Сертифицированный партнер</span>
+					<div class="certs__block-imgs">
+						<img src="img/serts/partner-1.jpg" alt="">
+						<img src="img/serts/partner-2.jpg" alt="">
+						<img src="img/serts/partner-3.jpg" alt="">
+						<img src="img/serts/partner-4.jpg" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="services-sec">
+	<div class="services container">
+		<div class="block-title">
+			<span>Услуги</span>
+			<p class="h1">Мои 4 основных направления</p>
+		</div>
+		<div class="services__item">
+			<div class="row">
+				<div class="grid-6">
+					<div class="services__img"></div>
+				</div>
+				<div class="grid-6 grid-12_s">
+					<div class="services__info">
+						<div class="services__title">
+							<img class="services__title-img" src="img/services/block-title-1.svg" alt="">
+							<p class="h3">Упаковка продуктов и услуг</p>
+							<div class="user-content">
+								<p>Как  вывести продукт на рынок? Я найду для вас УТП (критические и вторичные  факторы успеха), придумаю инновацию для рынка или создам маркет-микс.</p>
+								<p>На  выходе вы получите упакованные материалы для рекламы, маркетинговые  офферы, креативы, посадочные страницы и сайты, которую дают конверсию в  целевое действие.</p>
+								<p>Уже есть опыт продвижения с нуля  стабилизированных цветов, розы в колбе, множество франшиз, детских  товаров, развлекательных центров и многого другого.</p>
+							</div>
+							<div class="services__targets">
+								<span>Разработка лендингов</span>
+								<span>А/B тестирование</span>
+								<span>Фото- и видеосессия</span>
+								<span>Создание сайтов</span>
+								<span>Стратегии бренда</span>
+								<span>Брендинг</span>
+							</div>
+							<a href="#" class="btn btn--red fancy" data-src="#modal-order">Оставить заявку</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
 <?php include('footer.php'); ?>
